10a94249319790138c067b6a3e15ab25e07b6f85
JeffCheng <731590520@qq.com>
2023-08-23 22:41:39 +0800

    20230823

--- AcWing/LeetCode/lc006.java
+++ AcWing/LeetCode/lc006.java
@@ -1,19 +1,19 @@
 package AcWing.LeetCode;
 
-class Solution {
-    public String convert(String s, int numRows) {
-        if (numRows == 1) return s; //边界情况
+class Solution {    //模拟+思维题+等差数列
+    public String convert(String s, int n) {
+        if (n == 1) return s; //特判，边界情况，防止q=0死循环
         
         //每个字符遍历一次，时间复杂度O(n)
         String res = "";
-        int q = 2 * (numRows - 1);  //公差
-        for (int i = 0; i < numRows; i++) { //对每行分别处理
-            if (i == 0 || i == numRows-1) { //首行和尾行a1 = i, q = 2*(nrows-1)
+        int q = 2 * (n - 1);  //公差
+        for (int i = 0; i < n; i++) { //对每行分别处理
+            if (i == 0 || i == n-1) { //首行和尾行a1 = i, q = 2*(nrows-1)
                 for (int j = i; j < s.length(); j += q)
                     res += s.charAt(j);
             }
             else {
-                for (int j = i, k = 2 * numRows - i - 2; j < s.length() || k < s.length(); j += q, k += q) {
+                for (int j = i, k = 2 * n - i - 2; j < s.length() || k < s.length(); j += q, k += q) {
                     //第i行(0<i<n-1)，首项分别是a0=i, b0=2*n-i-2，两等差数列交替排列
                     if (j < s.length()) res += s.charAt(j);
                     if (k < s.length()) res += s.charAt(k);
---
+++ AcWing/LeetCode/lc077.java
@@ -0,0 +1,26 @@
+package AcWing.LeetCode;
+
+class Solution {    //DFS+固定顺序去重(start)+组合模板
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();     //存储路径
+
+
+    public List<List<Integer>> combine(int n, int k) {
+        dfs(n, k, 1);
+        return res;
+    }
+
+    //k代表还需要选几个数，start规定顺序，防止组合重复
+    void dfs(int n, int k, int start) {
+        if (k == 0) {
+            res.add(new ArrayList(path));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            path.add(i);
+            dfs(n, k - 1, i + 1);
+            path.remove(path.size() - 1);   //恢复现场
+        }
+    }
+}
