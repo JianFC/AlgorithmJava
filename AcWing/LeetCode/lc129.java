084ccb6a6719a549074bd2addbe70d58f53a16ae
JeffCheng <731590520@qq.com>
2023-09-13 10:15:54 +0800

    20230913

---
+++ AcWing/LeetCode/lc129.java
@@ -0,0 +1,32 @@
+package AcWing.LeetCode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {    //DFS + 维护信息
+    int ans;    //存储答案
+
+    public int sumNumbers(TreeNode root) {
+        if (root != null) dfs(root, 0);
+        return ans;
+    }
+
+    void dfs(TreeNode root, int number) {
+        number = number * 10 + root.val;    //更新number;
+        if (root.left == null && root.right == null) ans += number; //叶子节点
+        if (root.left != null) dfs(root.left, number);  //递归左子树
+        if (root.right != null) dfs(root.right, number);    //递归右子树
+    }
+}
---
+++ AcWing/LeetCode/lc130.java
@@ -0,0 +1,42 @@
+package AcWing.LeetCode;
+
+class Solution {    //Flood Fill算法 + DFS实现 + 逆向思维
+    char[][] board;
+    int n, m;
+    int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+
+    public void solve(char[][] _board) {
+        board = _board;
+        n = board.length;
+        if (n == 0) return ;
+        m = board[0].length;
+
+        for (int i = 0; i < n; i ++) {
+            if (board[i][0] == 'O') dfs(i, 0);  //第一列
+            if (board[i][m - 1] == 'O') dfs(i, m - 1);  //最后一列
+        }
+
+        for (int i = 0; i < m; i ++) {
+            if (board[0][i] == 'O') dfs(0, i);  //第一行
+            if (board[n - 1][i] == 'O') dfs(n - 1, i);  //最后一行
+        }
+
+        //根据标记更新board
+        for (int i = 0; i < n; i ++)
+            for (int j = 0; j < m; j ++)
+                if (board[i][j] == '#') board[i][j] = 'O';
+                else board[i][j] = 'X';
+        
+        _board = board;     //更新board
+        
+    }
+
+    void dfs(int x, int y) {    //逆向思维，遍历边界上的O能到的位置
+        board[x][y] = '#';  //标记，防止重复遍历，保证时间复杂度为O(n)
+        for (int i = 0; i < 4; i ++) {
+            int a = x + dx[i], b = y + dy[i];   //新位置
+            if (a >= 0 && a < n && b >= 0 && b < m && board[a][b] == 'O')
+                dfs(a, b);  //继续遍历
+        }
+    }
+}
