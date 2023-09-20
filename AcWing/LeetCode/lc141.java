a05d4c1bb7e163f6eb9aff3e76ea8b8b1221c56c
JeffCheng <731590520@qq.com>
2023-09-20 11:06:26 +0800

    20230920

---
+++ AcWing/LeetCode/lc141.java
@@ -0,0 +1,27 @@
+package AcWing.LeetCode;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {     //双指针之快慢指针问题
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;    //链表为空or只有一个点
+        ListNode s = head, f = head;   //s和f分别为慢指针和快指针
+        while (f != null) {
+            s = s.next; f = f.next; //快慢指针分别走一步
+            if (f == null) return false;    //走到null，没有环
+            f = f.next; //快指针再走一步
+            if (s == f) return true;    //快慢指针相遇，存在环，返回true
+        }
+
+        return false;
+    }
+}
---
+++ AcWing/LeetCode/lc142.java
@@ -0,0 +1,33 @@
+package AcWing.LeetCode;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {     //快慢指针 + 数学推导 + 思维题
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode s = head, f = head;    //定义快慢指针
+        while (f != null) {
+            s = s.next; f = f.next;     //快慢指针分别向后走一步
+            if (f == null) return null;
+            f = f.next; //f再走一步
+            if (s == f) {   //存在环
+                s = head;   //s从起点走
+                while (s != f) {
+                    s = s.next; f = f.next; //s和f分别向后走一步，相遇时即为入口，数学推导
+                }
+                return s;
+            }
+        }
+
+        return null;
+    }
+}
